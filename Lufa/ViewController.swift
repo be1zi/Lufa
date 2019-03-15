7c9da0990e221a7c8d6f05648e0584c970799ec7
Konrad Bełzowski <konrad.belzowski@comarch.pl>
2019-03-15 08:55:27 +0100

    First commit

---
+++ Lufa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lufa
+//
+//  Created by Konrad Belzowski on 15/03/2019.
+//  Copyright © 2019 Konrad Belzowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
