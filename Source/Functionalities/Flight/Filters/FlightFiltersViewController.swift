8ac10be3ef5b304d4198160e52191d927c25d82a
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-07-24 14:45:46 +0200

    flights filters navigation bar view

--- Source/Functionalities/Flight/Filters/FlightFiltersViewController.swift
+++ Source/Functionalities/Flight/Filters/FlightFiltersViewController.swift
@@ -7,11 +7,36 @@
 //
 
 import Foundation
+import UIKit
 
 class FlightFiltersViewController: BaseViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadTranslations() {
+        resetButton.title = "fligth.filters.reset".localized()
+    }
     
     // MARK: Appearance
     override func navigationBarTitle() -> String? {
         return "flight.filters.title".localized()
+    }
+    
+    // MARK: Actions
+    @IBAction func backButtonAction(_ sender: Any) {
+        view.endEditing(true)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func resetButtonAction(_ sender: Any) {
+    
     }
 }
